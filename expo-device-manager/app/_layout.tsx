50fb9ad3eb19f932264538122141968b9dba6075
da-stoaz <felix.steinberger@proton.me>
2026-02-10 11:23:53 +0100

    reset project

--- expo-device-manager/app/_layout.tsx
+++ expo-device-manager/app/_layout.tsx
@@ -1,24 +1,48 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import { Platform } from 'react-native';
+import { Tabs } from 'expo-router';
+import { House } from 'lucide-react-native';
+import { PlatformPressable } from '@react-navigation/elements';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+
+  const liquidGlass: boolean =
+    Platform.OS === 'ios' && parseInt(Platform.Version.split('.')[0], 10) >= 26;
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+  <>
+      {liquidGlass ? (
+        <NativeTabs>
+          <NativeTabs.Trigger name="index">
+            <Label>Home</Label>
+            <Icon sf="house.fill" md="home" />
+          </NativeTabs.Trigger>
+          <NativeTabs.Trigger name="settings">
+            <Icon sf="gear" md="settings" />
+            <Label>Settings</Label>
+          </NativeTabs.Trigger>
+        </NativeTabs >
+      ) : (
+        <Tabs
+          screenOptions={{
+
+            headerShown: true,
+            tabBarButton: (props) => (
+              <PlatformPressable
+                {...props}
+                onPress={(ev) => {
+                  props.onPress?.(ev);
+                }}
+              />
+            ),
+          }}>
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: 'Home',
+              tabBarIcon: ({ color }) => <House size={24} color={color} />,
+            }}
+          />
+
+          )
+      }
